e83e3e7ebcb1cffb05f2545fc19832a0882b63a3
shravanilalitha <86407447+shravanilalitha@users.noreply.github.com>
2021-06-24 15:15:08 +0530

    Add files via upload

---
+++ main (2).cs
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+      int a = 10;
+      int b = 20;
+      int sum = a + b;
+      string name="shravani";
+      Console.WriteLine(name);
+      string name1="reddy";
+    Console.WriteLine(name1 + " " +sum);
+    //Console.WriteLine(sum)
+  }
+}
